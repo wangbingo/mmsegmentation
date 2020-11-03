9d8bb81c9cad2b4b5b5a3f9f885c679228209cb7
王兵 <wangbing@101.1.68.92.in-addr.arpa>
2020-11-03 12:12:59 +0800

    train dataset is ADE20K

---
+++ mmseg/datasets/ade_my.py
@@ -0,0 +1,24 @@
+from .builder import DATASETS
+from .custom import CustomDataset
+
+
+@DATASETS.register_module()
+class ADE20KDataset(CustomDataset):
+    """ADE20K dataset.
+
+    In segmentation map annotation for ADE20K, 0 stands for background, which
+    is not included in 150 categories. ``reduce_zero_label`` is fixed to True.
+    The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
+    '.png'.
+    """
+    CLASSES = ('bg', 'road')
+
+    PALETTE = [[0, 0, 0], [255, 255, 255]]    # [255,255,255]white means forground
+
+    def __init__(self, **kwargs):
+        super(ADE20KDataset, self).__init__(
+            img_suffix='.png',
+            seg_map_suffix='.png',
+            # reduce_zero_label=True,
+            reduce_zero_label = False,
+            **kwargs)
