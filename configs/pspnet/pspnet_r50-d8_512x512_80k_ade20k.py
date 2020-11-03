d1e7e77b525aee5b86700da8c2943a0ffbde85b0
王兵 <wangbing@92-68-1-101.biz.kpn.net>
2020-11-03 11:16:01 +0800

    num_classes=2

--- configs/pspnet/pspnet_r50-d8_512x512_80k_ade20k.py
+++ configs/pspnet/pspnet_r50-d8_512x512_80k_ade20k.py
@@ -3,5 +3,5 @@ _base_ = [
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
 model = dict(
-    decode_head=dict(num_classes=150), auxiliary_head=dict(num_classes=150))
+    decode_head=dict(num_classes=2), auxiliary_head=dict(num_classes=2))
 test_cfg = dict(mode='whole')
