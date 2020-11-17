4884e418e5a448905d5d971caf5c520fc2ba9918
王兵 <wangbing@92-68-1-103.biz.kpn.net>
2020-11-18 01:28:36 +0800

    crop_size = (256, 256)

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -3,11 +3,11 @@ dataset_type = 'CityscapesDataset'
 data_root = '../../input/256x256/'     #####
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 512)                 ########
+crop_size = (256, 256)                 ########
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(1024, 512), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(512, 256), ratio_range=(0.5, 2.0)),      ######
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1024, 512),
+        img_scale=(512, 256),                         #####
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
@@ -32,7 +32,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=4,                ############
+    samples_per_gpu=8,                ############
     workers_per_gpu=2,         
     train=dict(
         type=dataset_type,
