406bd049562c5993ad00627f688d01560347457f
王兵 <wangbing@92-68-1-103.biz.kpn.net>
2020-11-18 01:01:38 +0800

    data_root = '../input/256x256/'

--- configs/_base_/datasets/cityscapes.py
+++ configs/_base_/datasets/cityscapes.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'CityscapesDataset'
-data_root = 'data/'     #####
+data_root = '../input/256x256/'     #####
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (512, 512)                 ########
@@ -32,23 +32,23 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=6,                ############
+    samples_per_gpu=4,                ############
     workers_per_gpu=2,         
     train=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='train/images',
-        ann_dir='train/labels',
+        img_dir='train',
+        ann_dir='masks',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='val/images',
-        ann_dir='val/labels',
+        img_dir='train',
+        ann_dir='masks',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         data_root=data_root,
-        img_dir='val/images',
-        ann_dir='val/labels',
+        img_dir='train',
+        ann_dir='msaks',
         pipeline=test_pipeline))
