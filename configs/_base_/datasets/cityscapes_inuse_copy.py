fb63e8474db9a24319364b862ffd8b11c2496cc1
王兵 <wangbing@92-68-1-106.biz.kpn.net>
2020-11-22 00:52:45 +0800

    backup 2 files

---
+++ configs/_base_/datasets/cityscapes_inuse_copy.py
@@ -0,0 +1,56 @@
+# dataset settings
+dataset_type = 'CityscapesDataset'
+data_root = '../../input/256x256/'     #####
+img_norm_cfg = dict(
+    # mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)   ### ori
+    mean=[175.099, 147.099, 181.427], std=[55.732, 71.613, 53.056], to_rgb=True)   ### kidney
+crop_size = (256, 256)                 ########
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(512, 256), ratio_range=(0.5, 2.0)),      ######
+    dict(type='RandomRotate', prob=0.5, degree=(10., 20., 30.)),            ######
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 256),                         #####
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=32,                ############
+    workers_per_gpu=2,         
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train',
+        ann_dir='masks',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train',
+        ann_dir='masks',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='train',
+        ann_dir='msaks',
+        pipeline=test_pipeline))
